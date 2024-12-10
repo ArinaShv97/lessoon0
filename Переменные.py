c1ffc37d9138ace9a5269c00635769d1d2271fb3
ArinaShv97 <arina.kvochko@gmail.com>
2024-12-10 15:40:04 +0500

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,12 @@
+kol_dz = 12
+kol_time = 1.5
+name_ed = 'Python'
+time_one = (( kol_time) / (kol_dz))
+print('time_one:',(time_one ))
+
+print ('Курс:',( name_ed))
+print('всего задач:', (kol_dz))
+print('затрачено часов:', (kol_time))
+print('среднее время выполнения', (time_one))
+
+# не удалось вывести одним выражениме print (('Курс:',( name_ed)) and ('всего задач:', (kol_dz)) and ('затрачено часов:', (kol_time)) and ('среднее время выполнения:', (time_one)))
